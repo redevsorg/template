8d5f5217d4a93fed739dc536169690c7dfbad03b
sahith <78869400+sahith-p@users.noreply.github.com>
2024-04-01 13:41:06 -0700

    changes

--- src/directus/requests/home.ts
+++ src/directus/requests/home.ts
@@ -4,6 +4,7 @@ import { readItems } from "@directus/sdk";
 export async function getHomeData(): Promise<Record<string, any>[]> {
   await client.login(process.env.CMS_EMAIL as string, process.env.CMS_PASSWORD as string);
   const cards = await client.request(
+    // @ts-ignore
     readItems('template_home_cards', {
       fields: ['*'],
     })

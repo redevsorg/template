b46824b71975c04e0a8159356e4eadabb0effd9e
sahith <78869400+sahith-p@users.noreply.github.com>
2024-04-01 13:34:51 -0700

    directsuy

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -4,10 +4,8 @@ export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const secret = searchParams.get('secret');
 	const page = searchParams.get('page');
-    console.log(page);
-    console.log(secret);
 
-	if (secret !== process.env.CMS_TOKEN) {
+	if (secret != process.env.CMS_SECRET) {
 		return new Response('Invalid token', { status: 401 });
 	}
 
